353b987a6abf4b9186086eeea9ff02c0da1e6c84
jihye yu <dbwlgp1yng@gmail.com>
2023-08-30 16:07:38 +0900

    design: error 스타일 지정

---
+++ src/pages/Error.styled.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledError = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 4rem;
+  p {
+    margin-top: 2rem;
+  }
+  font-size: 2rem;
+  color: #999;
+`;
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,14 @@
+import { StyledError } from "./Error.styled";
+import errorImage from "../images/error.jpg"; 
+
 export default function Error( ){
   return (
-    <>Error</>
+    <StyledError>
+      <img
+        src={errorImage} 
+        alt="github Error"
+      />
+      <p>Error! This is not the webpage you are looking for.</p>
+    </StyledError>
   );
 }
